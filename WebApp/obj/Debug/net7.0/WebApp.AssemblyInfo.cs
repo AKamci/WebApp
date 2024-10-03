7f9f61ea40b145b048ed8e3bffeb3af0ced3bd89
abdulkadir <abdulkadir.kamci10@gmail.com>
2024-10-03 19:26:53 +0300

    test

--- WebApp/obj/Debug/net7.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net7.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae130e334ac0bd8aaf9c080e6303d4aa1b3bd2dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+881f3f229bff6eefe18ce0b561b062618c258eec")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
